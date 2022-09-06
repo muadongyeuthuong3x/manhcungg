c72b583bd2fc087378bce149ff218266a940aa79
linhnv2nm <muadongyeuthuong3x@gmail.com>
2022-09-06 22:47:00 +0700

    first commit

---
+++ src/Login.js
@@ -0,0 +1,27 @@
+
+import './register.css';
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+    const navigate = useNavigate();
+    function handleClick() {
+        navigate("/register");
+      }
+    return (
+        <div>
+            <h1>Đây là trang dang nhap</h1>
+            <label htmlFor="">user name</label>
+            <input type="text" id="fname" name="fname" placeholder="user name" />
+            <br />
+            <label htmlFor="">Password</label>
+            <input type="password" id="fname" name="fname" placeholder="password" />
+            <br />
+            <div className="gr-button">
+                <button className="bt1">Login</button>
+                <button onClick={handleClick}>Register</button>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
